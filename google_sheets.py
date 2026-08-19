91e0ed2d3f18b5e45892b242b2fc66c7b0be53c9
martademchyshak-coder <marta.demchyshak@gmail.com>
2026-08-19 21:44:07 +0300

    Update google_sheets.py

--- google_sheets.py
+++ google_sheets.py
@@ -297,8 +297,8 @@ def find_manager_row_for_day(
     if day_code in _manager_day_cells_cache:
         manager_cells = _manager_day_cells_cache[day_code]
     else:
-    with _sheet_lock:
-        manager_cells = worksheet.get(
+        with _sheet_lock:
+            manager_cells = worksheet.get(
             f"D{start_row}:D{end_row}"
         )
 
